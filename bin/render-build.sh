22a18c445e3c6ffda588635f0a997a29aed222f4
masi <masi@fozzy.com>
2024-05-02 01:05:38 +0300

    add drop on test server

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 bundle config set --local without development
 bundle install
-bundle exec rails db:drop db:create db:migrate db:seed
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
+bundle exec rails db:create db:migrate db:seed
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
