9f505dcb9790b441d9b981d07fb17826ba269aa3
masi <masi@fozzy.com>
2024-05-09 13:34:44 +0300

    fixed breadcrumb link

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ class Post < ApplicationRecord
 
   validates :body, presence: true, length: { minimum: 100, maximum: 4000 }
   validates :title, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :category_id, presence: true
 
   scope :user_like, ->(user_id) { likes.where(user_id:) }
   scope :with_association, -> { includes(:category, :creator).order(created_at: :desc) }
