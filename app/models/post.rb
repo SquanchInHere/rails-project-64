91419a118b5d701825888a7bdc7ca76bc09d6ca3
masi <masi@fozzy.com>
2024-05-09 14:45:26 +0300

    fixed text

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,8 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
 
-  validates :body, presence: true, length: { minimum: 100, maximum: 4000 }
-  validates :title, presence: true, length: { minimum: 5, maximum: 255 }
-  validates :category_id, presence: true
+  validates :body, length: { minimum: 100, maximum: 4000 }
+  validates :title, length: { minimum: 5, maximum: 255 }
 
   scope :user_like, ->(user_id) { likes.where(user_id:) }
   scope :with_association, -> { includes(:category, :creator).order(created_at: :desc) }
